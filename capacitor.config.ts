8e1fad67fa615e237ed8b1b8ffea4e07d853df64
deno555 <gaaldenis@gmail.com>
2022-03-29 15:08:15 +0200

    commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'h2-alza-3',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
