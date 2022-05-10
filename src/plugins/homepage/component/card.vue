d9a5d73874a2d46574745a58818d0eab8dec332e
deno555 <gaaldenis@gmail.com>
2022-05-10 12:40:40 +0200

    mensi fix

--- src/plugins/homepage/component/card.vue
+++ src/plugins/homepage/component/card.vue
@@ -1,5 +1,18 @@
 <template>
-    <div>
-        
-    </div>
+        <div class="nobull" v-for="(i) in card" :key="i.cardTitle">
+          <ion-card>
+            <ion-card-header>
+              <img width="85" class="imgs" src="https://cdn.discordapp.com/attachments/768503373363806218/970768614942732388/unknown.png">
+              <img width="25" top="100px" class="imgs" src="https://cdn.discordapp.com/attachments/768503373363806218/970968283715096586/unknown.png">
+            </ion-card-header>
+
+            <ion-card-content>
+              <p>Impostor</p>
+              <p>Sus</p>
+              <p>C#</p>
+              <p>Tvoju mamu</p>
+              <p>Vesmir</p>
+            </ion-card-content>
+          </ion-card> 
+      </div>
 </template>
--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -1,44 +1,16 @@
 <template>
-  <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title class="ion-text-center">Stážisti</ion-title>
-        <ion-button><ion-icon name="options-outline"></ion-icon></ion-button>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content>
-
-      <div class="nobull" v-for="(i) in card" :key="i.cardTitle">
-          <ion-card>
-            <ion-card-header>
-              <img width="85" class="imgs" src="https://cdn.discordapp.com/attachments/768503373363806218/970768614942732388/unknown.png">
-              <img width="25" top="100px" class="imgs" src="https://cdn.discordapp.com/attachments/768503373363806218/970968283715096586/unknown.png">
-            </ion-card-header>
-
-            <ion-card-content>
-              <p>Impostor</p>
-              <p>Sus</p>
-              <p>C#</p>
-              <p>Tvoju mamu</p>
-              <p>Vesmir</p>
-            </ion-card-content>
-          </ion-card> 
-      </div>
-
-    <ion-button @click="addCard()">Test</ion-button>
-
-    </ion-content>
-  </ion-page>
-
+    <Homepage/>
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue';
-  import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent} from '@ionic/vue';
+  import Homepage from '@/plugins/homepage/view/homepage.vue';
 
   export default  defineComponent({
     name: 'Tab1Page',
-    components: {IonHeader, IonToolbar, IonTitle, IonContent, IonPage},
+    components:{
+      Homepage
+    },
 
     data(){
       return{
@@ -56,53 +28,25 @@
           {cardID:3},
           {cardID:4},
           {cardID:5},
-          ]
+          ],
+        cardName:[
+          {names:"Impostor"},
+          {names:"Crewmate1"},
+          {names:"Crewmate2"},
+          {names:"Crewmate3"},
+          {names:"Crewmate4"},
+          {names:"Crewmate5"},
+          ],
       };
     },
 
     methods:{
-        // addName(){
-        //   this.card.push({
-        //     nameText:this.name,
-        //   }),
-        //   this.name=''
-        // },
-
-        // addJob(){
-        //   this.card.push({
-        //     jobText:this.job,
-        //   }),
-        //   this.job=''
-        // },
-
-        // addSkill(){
-        //   this.card.push({
-        //     skillText:this.skillsInJob,
-        //   }),
-        //   this.skillsInJob=''
-        // },
-
-        // addMoney(){
-        //   this.card.push({
-        //     moneyNum:this.money,
-        //   }),
-        //   this.money=''
-        // },
-
-        // addCity(){
-        //   this.card.push({
-        //     cityText:this.city,
-        //   }),
-        //   this.city=''
-        // },
-
-        addCard(cardID){
-          this.card.push({
-            cardID:cardID+1
-          })
-        },
-
-      }
+      addCard(cardID:number){
+        this.card.push({
+          cardID:cardID+1
+        }),
+      },
+    }, 
   })
 
 
