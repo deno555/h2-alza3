1cb58115bedfea3034c100fe112d82b1ffc83b6b
Pepeeeeeeeeee <duga.peter.ml@gmail.com>
2022-05-10 18:10:13 +0200

    all fixed

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -1,20 +1,24 @@
 <template>
   <ion-page>
-    <ion-content>
-      <ion-header>
-        <!-- import <AHeader /> component -->
+    <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 2</ion-title>
+        </ion-toolbar>
       </ion-header>
-        <AADetailsPage />
-    </ion-content>
-  </ion-page>  
+    <ion-content :fullscreen="true">  
+      
+      <AADetailsPage />
+      
+      </ion-content>
+  </ion-page>
 </template>
 
 <script lang="ts">
-  import AADetailsPage from '@/plugins/detailPage/view/AADetailsPage.vue';
-  import { IonPage, IonContent, IonHeader} from '@ionic/vue';
+import AADetailsPage from '@/plugins/detailPage/view/AADetailsPage.vue';
+import { IonPage, IonContent, IonHeader, IonToolbar, IonTitle,} from '@ionic/vue';
   // import AHeader \import header2 from '@/plugins/global_component/AHeader.vue'\ 
 
   export default{
-      components: {AADetailsPage, IonPage, IonContent, IonHeader} // pridat 'AHeader'
+      components: {AADetailsPage, IonPage, IonContent, IonHeader, IonToolbar, IonTitle,} // pridat 'AHeader'
   }
 </script> 
